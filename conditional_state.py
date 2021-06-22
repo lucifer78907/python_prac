1dbef972b6f79739be556c23f1e9212cb5e1bb49
Rudra <chaudhary66667@gmail.com>
2021-06-22 09:09:39 +0530

    Conditional op

---
+++ conditional_state.py
@@ -0,0 +1,10 @@
+my_input=input("Enter a fruit").lower()
+if my_input=="apple":
+    print("You entered apple")
+elif my_input=="bananas":
+    print("You entered bananas")
+elif my_input=="pear":
+    print("you entered pear")
+    print("You entered reyna ka bachha")
+else:
+    print(f"You entered {my_input}")
