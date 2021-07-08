6706734975272f34ad9615e40951cf1b275e3472
Sonal <2122002sonal@gmail.com>
2021-07-08 09:11:50 +0530

    notes till f-string

---
+++ f-string{}.py
@@ -0,0 +1,5 @@
+name = input("Enter your name")
+age = int(input("enter your age"))#this variable "age" will take input as int datatype as specified
+print(f"Hello, My name is {name} and I'm {age} years old.")
+#here name and age will get replaced with the above input values (the user will provide here)
+
---
+++ input().py
@@ -0,0 +1,3 @@
+x = input('Enter your name:')
+print('Hello,' + x) #here users input will get added with this "Hello," message.
+
